ecf67bfc7685279149be2f68d26bc328439305ef
Artem Lomakin <Lomakin-as@mail.ru>
2022-05-13 19:20:19 +0300

    add: Article create post in controller and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def create 
-    render plain: params[:article].inspect
+    @article = Article.new(params_article)
+    if @article.valid?
+      @article.save
+    else 
+      # render "new"
+      render action: "new"
+    end
+
+    # render plain: params[:article].inspect
+  end
+
+  private
+
+  def params_article 
+    params.require(:article).permit(:title, :text)
   end
 end
